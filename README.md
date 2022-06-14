7ecc6eaaa7f056e8a1f92692af04f5289655a6b8
dofine <dofine@users.noreply.github.com>
2022-06-14 16:44:25 +0800

    tests: add more tests.

--- README.md
+++ README.md
@@ -6,6 +6,16 @@
 - [逐天天气预报](https://dev.qweather.com/docs/api/weather/weather-daily-forecast/)
 - [实时空气质量](https://dev.qweather.com/docs/api/air/air-now/)
 
+## Usage
+暂时只支持异步（async）调用，因为最初是为集成在 homeassistant 开发的 :)
+
+### 实时天气
+```python
+import aiohttp
+
+with aiohttp
+```
+
 
 ## TODO
 
--- tests/test_python_qweather.py
+++ tests/test_python_qweather.py
@@ -1,5 +1,19 @@
 from python_qweather import __version__
+from python_qweather import QWeather
+import aiohttp
+
+import pytest
+import os
 
 
 def test_version():
-    assert __version__ == "0.1.1"
+    assert __version__ == "0.1.3"
+
+@pytest.mark.asyncio
+async def test_now_weather():
+    async with aiohttp.ClientSession() as client_session:
+        q = QWeather(api_key=os.environ['QWEATHER_APIKEY'], session=client_session, location_key='101010100')
+        now_weather = await q.async_get_now_weather()
+        assert now_weather['code'] == '200'
+        # assert now_weather['now']['temp'] == '25'
+        
